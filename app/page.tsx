c107ca981d3f5a28fb349d4846bc5c317dcd915c
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-05 12:57:54 +0330

    added layout and home page files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// Exporting home page (functional component) as default
+export default function HomePage():JSX.Element {
+    // Returning JSX
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
