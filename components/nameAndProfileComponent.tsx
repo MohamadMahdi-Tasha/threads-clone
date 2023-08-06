3aea57c8b955fc133d97ad4bfa19478dae313233
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-06 12:32:47 +0330

    added link to name in top side of user info

--- components/nameAndProfileComponent.tsx
+++ components/nameAndProfileComponent.tsx
@@ -1,3 +1,6 @@
+// Importing Part
+import Link from "next/link";
+
 // Defining type of props
 interface typeOfProps {
     title: string,
@@ -14,7 +17,7 @@ export default function NameAndProfileComponent({title,id,img}:typeOfProps):JSX.
                 <h1 className={'text-white font-bold text-2xl mb-2'}>{title}</h1>
                 <div className={'flex items-center gap-3'}>
                     <h6 className={'text-white font-normal'}>{id}</h6>
-                    <a className={'bg-gray-700/30 text-gray-400 px-[10px] py-[3px] rounded-3xl text-xs'} href="#">threads.net</a>
+                    <Link href={'/'} className={'bg-gray-700/30 text-gray-400 px-[10px] py-[3px] rounded-3xl text-xs'}>Threads clone</Link>
                 </div>
             </div>
             <img className={'w-[100px] h-[100px] rounded-full'} src={img} alt="sylie.jahed"/>
