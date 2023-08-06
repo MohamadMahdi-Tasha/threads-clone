7956253b6f287c983efb1d76335bc57227649bf4
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-06 13:23:17 +0330

    added profile modal component

--- components/nameAndProfileComponent.tsx
+++ components/nameAndProfileComponent.tsx
@@ -1,5 +1,10 @@
+// Making nextJS to render this component in client side
+'use client';
+
 // Importing Part
 import Link from "next/link";
+import {useState} from "react";
+import ModalComponent from "@/chunks/modalComponent";
 
 // Defining type of props
 interface typeOfProps {
@@ -10,6 +15,9 @@ interface typeOfProps {
 
 // Exporting name and profile component as default
 export default function NameAndProfileComponent({title,id,img}:typeOfProps):JSX.Element {
+    // Defining state of component
+    const [isProfileModalOpened, setProfileModalOpened] = useState(false);
+
     // Returning JSX
     return (
         <div className={'flex items-center justify-between gap-5 mb-5'}>
@@ -20,7 +28,10 @@ export default function NameAndProfileComponent({title,id,img}:typeOfProps):JSX.
                     <Link href={'/'} className={'bg-gray-700/30 text-gray-400 px-[10px] py-[3px] rounded-3xl text-xs'}>Threads clone</Link>
                 </div>
             </div>
-            <img className={'w-[100px] h-[100px] rounded-full'} src={img} alt="sylie.jahed"/>
+            <img onClick={() => setProfileModalOpened(true)} className={'w-[100px] h-[100px] rounded-full btn-animation cursor-pointer'} src={img} alt="sylie.jahed"/>
+            <ModalComponent isOpened={isProfileModalOpened} closeFunction={() => setProfileModalOpened(false)}>
+                <img className={'w-[200px] h-[200px] rounded-full'} src={img} alt="sylie.jahed"/>
+            </ModalComponent>
         </div>
     );
 }
