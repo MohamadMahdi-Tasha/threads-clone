970bf90ee9e3bd597ccec0fbe242818abd4dbe58
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-06 12:12:50 +0330

    added hover styles to drop down button components

--- chunks/dropdownButtonComponent.tsx
+++ chunks/dropdownButtonComponent.tsx
@@ -8,7 +8,7 @@ interface propsTypes {
 export default function DropdownButtonComponent({children, textIsRed}:propsTypes):JSX.Element {
     // Returning JSX
     return (
-        <button className={`text-start whitespace-nowrap p-[15px] text-sm ${(textIsRed) ? 'text-red-600' : 'text-white'}`}>
+        <button className={`text-start whitespace-nowrap bg-transparent hover:bg-black/20 transition-all p-[15px] text-sm ${(textIsRed) ? 'text-red-600' : 'text-white'}`}>
             {children}
         </button>
     );
