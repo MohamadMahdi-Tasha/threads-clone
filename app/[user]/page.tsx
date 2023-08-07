af4fcd91f4052d8bd177b863e84b191e7c689f60
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-07 13:29:33 +0330

    added some dummy json info

--- app/[user]/page.tsx
+++ app/[user]/page.tsx
@@ -1,4 +1,5 @@
 // Importing Part
+import { ThreadsAPI } from 'threads-api';
 import HolderComponent from "@/chunks/holderComponent";
 import ThemeTogglerComponent from "@/components/themeTogglerComponent";
 import ThreadComponent from "@/components/threadComponent";
@@ -9,20 +10,16 @@ import UserPageDownloadComponent from "@/components/userPageDownloadComponent";
 
 // Exporting user page (functional component) as default
 export default function UserPage():JSX.Element {
+    // Using hooks
+    const threadsAPI = new ThreadsAPI();
+
     // Returning JSX
     return (
         <HolderComponent small>
             <header className={'py-[20px] flex items-center justify-center'}><ThemeTogglerComponent /></header>
             <section className={'mb-10'}>
                 <header>
-                    <UserInfoComponent
-                        title={'بلاگر درسي | ساينا شيخ نواز جاهد ( سايلي)'}
-                        id={'sylie.jahed'}
-                        img={'https://picsum.photos/100/100'}
-                        content={"#سایلی👩‍⚕️ هستمبا من همراه باش تا برای زندگیمون #برنامه_ریزی کنیمو شايد فراتر بريم..🚀سفارش دفترشطرنجي سايلي🗒️👇🏻"}
-                        followersCount={35000}
-                        link={'https://www.google.com'}
-                    />
+                    <UserInfoComponent id={'sylie.jahed'}/>
                     <ThreadsOrRepliesComponent activePart={'threads'} id={'sylie.jahed'} />
                 </header>
                 <main className={'[&>div:not(:last-of-type)]:border-b [&>div:not(:last-of-type)]:pb-[15px] [&>div:not(:last-of-type)]:border-b-barcelonaMediaOutline flex flex-col gap-5'}>
--- components/userInfoComponent.tsx
+++ components/userInfoComponent.tsx
@@ -1,32 +1,27 @@
 // Defining type of props
 import NameAndProfileComponent from "@/components/nameAndProfileComponent";
 import BioComponent from "@/components/bioComponent";
+import {ThreadsAPI} from "threads-api";
 import InfoBottomComponent from "@/components/infoBottomComponent";
 
 interface typeOfProps {
-    title: string;
     id: string;
-    img: string;
-    content: string;
-    followersCount: number;
-    link?: string;
 }
 
 // Exporting userInfo component as default
-export default function UserInfoComponent({
-    title,
-    id,
-    img,
-    content,
-    followersCount,
-    link
-}:typeOfProps):JSX.Element {
+export default function UserInfoComponent({id}:typeOfProps):JSX.Element {
+    // Using threads api
+    const threadsAPI = new ThreadsAPI();
+    const username = '_junhoyeo';
+    const userID =  threadsAPI.getUserIDfromUsername(username);
+    console.log(userID)
+
     // Return JSX
     return (
         <div className={'mb-5'}>
-            <NameAndProfileComponent title={title} img={img} id={id}/>
-            <BioComponent content={content}  />
-            <InfoBottomComponent id={id} followersCount={followersCount} link={link} />
+            <NameAndProfileComponent title={'title'} img={'img'} id={id}/>
+            <BioComponent content={'content'}  />
+            <InfoBottomComponent id={id} followersCount={2} link={'link'} />
         </div>
     );
 }
